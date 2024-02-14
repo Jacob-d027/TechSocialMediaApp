e218247f213480eb4f31014ebedc568c70553c45
Jacob Davis <jacobdofficial@gmail.com>
2024-02-14 09:21:27 -0700

    Updated constraints to postListTableView to make sure they're actually behaving as expected. Finally they are and now I move on to display the specific posts the current user has made and give them the option to edit them.

--- techSocialMediaApp/PostListTableViewController.swift
+++ techSocialMediaApp/PostListTableViewController.swift
@@ -27,12 +27,8 @@ class PostListTableViewController: UITableViewController {
             
         }
         
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        tableView.rowHeight = UITableView.automaticDimension
+        
     }
 
     // MARK: - Table view data source
@@ -59,9 +55,6 @@ class PostListTableViewController: UITableViewController {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 
-//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-//        <#code#>
-//    }
     
     
     // MARK: - Navigation
